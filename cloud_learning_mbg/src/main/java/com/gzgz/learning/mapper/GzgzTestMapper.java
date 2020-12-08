2c4a1e44a79525cb42b060a1051f4805952b286a
1024262077@qq.com <pzl688pzl>
2020-12-08 09:26:11 +0800

    shiro学习

---
+++ cloud_learning_mbg/src/main/java/com/gzgz/learning/mapper/GzgzTestMapper.java
@@ -0,0 +1,7 @@
+package com.gzgz.learning.mapper;
+
+import com.gzgz.learning.model.GzgzTest;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface GzgzTestMapper extends Mapper<GzgzTest> {
+}
---
+++ cloud_learning_shiro/src/main/java/com/gzgz/learning/LeanringShiroMain.java
@@ -0,0 +1,18 @@
+package com.gzgz.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+/**
+ * @ClassName: LeanringShiroMain
+ * @Description:
+ * @Author: pzl
+ * @CreateDate: 2020/12/7 17:47
+ * @Version: 1.0
+ */
+@SpringBootConfiguration
+public class LeanringShiroMain {
+    public static void main(String[] args) {
+        SpringApplication.run(LeanringShiroMain.class,args);
+    }
+}
